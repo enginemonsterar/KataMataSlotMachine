f501a8f0954d9c31ff934f92e1e0fbc4c0e7a10d
Januar Elsan <januarelsan@gmail.com>
2019-09-24 13:56:05 +0700

    Update SlotManager

--- Assets/Scripts/SlotManager.cs
+++ Assets/Scripts/SlotManager.cs
@@ -1,17 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using MonsterAR.Utility;
 
 public class SlotManager : Singleton<SlotManager>
 {
     // Start is called before the first frame update
     [SerializeField] private Slot[] slots;
+    [SerializeField] private Sprite[] prizeSprites;
 
     // Start is called before the first frame update
     void Start()
     {
-                
+        for (int i = 0; i < slots.Length; i++)
+        {
+            slots[i].transform.GetChild(0).GetChild(0).GetChild(i).GetChild(0).GetComponent<Image>().sprite = prizeSprites[i];            
+        }
     }
 
     IEnumerator RollingSlots(){
