407cade64b1e506049cac49a3683402a1cf74356
Januar Elsan <januarelsan@gmail.com>
2019-09-24 13:46:36 +0700

    Upload First Project

---
+++ Assets/Scripts/Slot.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slot : MonoBehaviour
+{
+
+    private GameObject content;
+    private bool rolling;
+
+    private GameObject resultSlotItem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        content = transform.GetChild(0).GetChild(0).gameObject;
+        resultSlotItem = transform.GetChild(0).GetChild(1).gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(rolling){
+            content.transform.position = new Vector3(
+                content.transform.position.x,
+                content.transform.position.y + 20,
+                content.transform.position.z
+            );
+        }
+    }
+
+    public void StartRolling(){
+        rolling = true;
+        StartCoroutine(Rolling());
+    }
+
+    public void StopRolling(){
+        rolling = false;
+        resultSlotItem.SetActive(true);
+        content.SetActive(false);
+        
+    }
+
+    IEnumerator Rolling(){
+        resultSlotItem.SetActive(false);
+        content.SetActive(true);
+        
+        yield return new WaitForSeconds(5);        
+        StopRolling();
+    }
+
+    
+}
---
+++ Assets/Scripts/SlotManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MonsterAR.Utility;
+
+public class SlotManager : Singleton<SlotManager>
+{
+    // Start is called before the first frame update
+    [SerializeField] private Slot[] slots;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+                
+    }
+
+    IEnumerator RollingSlots(){
+        for (int i = 0; i < slots.Length; i++)
+        {
+            slots[i].StartRolling();            
+            yield return new WaitForSeconds(0.3f);   
+        }
+    }
+
+    public void GoRolling(){
+        StartCoroutine(RollingSlots());
+    }
+}
